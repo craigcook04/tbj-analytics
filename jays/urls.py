5a5d2ab12c5609c3c3b3e48296647c7ae56f15e8
craigcook04 <ccook87@uwo.ca>
2019-09-02 22:50:16 -0400

    updated README

--- jays/urls.py
+++ jays/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('jays/', include('jays_app.urls')),
+    path('', include('jays_app.urls')),
     path('admin/', admin.site.urls),
 ]
